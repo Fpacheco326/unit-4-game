c4994b242f0feaddf83de92f846b9a618d61a475
Fernando Pacheco <fpacheco326@gmail.com>
2019-01-26 23:38:32 -0800

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,53 @@
 $(function () {
-  // $('#GemButtons').on('click', function () {});
 
+
+  var compGem = getRandomNumber(19, 120);
+  var goal = 0;
   var redGem = getRandomNumber(1, 12);
   var blueGem = getRandomNumber(1, 12);
   var greenGem = getRandomNumber(1, 12);
   var yellowGem = getRandomNumber(1, 12);
   var totalScore = 0;
+  var wins = 0;
+  var losses = 0;
+ 
+
+
+  function gameReset() {
+    compGem = getRandomNumber(19, 120);
+    $("#compGem").text(compGem);
+    redGem = getRandomNumber(1, 12);
+    blueGem = getRandomNumber(1, 12);
+    greenGem = getRandomNumber(1, 12);
+    yellowGem = getRandomNumber(1, 12);
+    totalScore = 0;
+    $("#TotalScore").text(totalScore);
+  
+  }
+  
+  function win() {
+    alert("You Win!");
+    wins++;
+    $("#wins").text(wins);
+    gameReset();
+  }
+  
+  function lose() {
+    alert("You Lose!");
+    losses++;
+    $("#losses").text(losses);
+    gameReset();
+  }
+  fillWins = document.querySelector('#wins').innerHTML = wins;
+  fillLosses = document.querySelector('#losses').innerHTML = losses;
+  console.log(wins)
+
+
+  $('#CompGem').one('click', function () {
+    goal += compGem;
+    $('#Goal').text(goal);
+
+  });
 
   $('#RedGem').on('click', function () {
     totalScore += redGem;
@@ -32,4 +74,9 @@ $(function () {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+
+
+
+
 })
